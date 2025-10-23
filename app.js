41c5cd2ad0f2dff7cc163b23ed0a96aade1081ea
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-10-23 13:24:59 -0700

    Update: added long, logoff

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const errorHandler = require("./middleware/error-handler");
 const notFound = require("./middleware/not-found");
-const { storedUsers, setLoggedOnUser } = require("./util/memoryStore");
+
+const userRouter = require("./routes/user");
+
 const app = express();
 
 app.use(express.json({ limit: "1kb" }));
@@ -14,32 +16,25 @@ app.use((req, res, next) => {
 })
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.json({message: "Hello World."})
 });
 
 app.post("/testpost", (req, res) => {
   res.json({message: "Everything Worked."});
-});
+})
 
-app.post("/user", (req, res)=>{
-    const newUser = {...req.body}; // this makes a copy
-    storedUsers.push(newUser);
-    setLoggedOnUser(newUser);  // After the registration step, the user is set to logged on.
-    delete req.body.password;
-    res.status(201).json(req.body);
-});
+app.use("/user", userRouter);
 
 app.use(errorHandler);
 app.use(notFound);
 
-
 const port = process.env.PORT || 3000;
 try {
-  app.listen(port, () =>
-    console.log(`Server is listening on port ${port}...`),
-  );
+    app.listen(port, () =>
+        console.log(`Server is listening on port ${port}...`),
+    );
 } catch (error) {
-  console.log(error);
+    console.log(error);
 }
 
 let isShuttingDown = false;
---
+++ controllers/userController.js
@@ -0,0 +1,29 @@
+const { StatusCodes } = require("http-status-codes");
+const { storedUsers, setLoggedOnUser } = require("../util/memoryStore");
+
+const register = (req, res) => {
+    const newUser = {...req.body};
+    storedUsers.push(newUser);
+    setLoggedOnUser(newUser);
+    delete req.body.password;
+    res.status(StatusCodes.CREATED).json(req.body);
+}
+
+const logonUser = (req, res) => {
+    const { email, password} = req.body;
+    const currentUser = storedUsers.find((user) => user.email === email);
+
+    if (!currentUser || currentUser.password !== password) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({ message: "Authentication Failed" });
+    }
+    setLoggedOnUser(currentUser);
+    res.status(StatusCodes.OK).json({ name: currentUser.name });
+
+}
+
+const logOff = (req, res) => {
+    setLoggedOnUser(null);
+    res.sendStatus(StatusCodes.OK);
+}
+
+module.exports = { register, logonUser, logOff };
