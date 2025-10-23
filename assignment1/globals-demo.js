34d199683c87ad35278a2ea56f0e79b5687e44d4
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-10-23 10:37:13 -0700

    Initial: global demo: added filename nad dirname

--- assignment1/globals-demo.js
+++ assignment1/globals-demo.js
@@ -1,7 +1,13 @@
 // Log __dirname and __filename
+console.log('Directory:', __dirname) //Remember two underscores
 
+console.log('File:', __filename)
 
 // Log process ID and platform
+console.log('Process ID', process.pid)
 
+console.log('Process Platform', process.platform)
 
 // Attach a custom property to global and log it
+global.x = 'Global Property'
+console.log(x)
