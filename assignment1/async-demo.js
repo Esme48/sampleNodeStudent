78a4ada6544253f3e39e2374b4771a31ebbecc26
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-10-23 10:39:24 -0700

    Initial: async demonstration

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -1,18 +1,45 @@
 const fs = require('fs');
 const path = require('path');
+const fsPromises = require('fs').promises;
 
 
 // Write a sample file for demonstration
 
-// 1. Callback style
+const filePath = path.join(__dirname, 'tmpFile.txt')
 
+fs.writeFileSync(filePath, 'File Exists')
+
+// 1. Callback style
+fs.readFile(filePath, 'utf8', (err, fileHandle) => {
+  if (err) {
+    console.log('File failed to open', err)
+  } else {
+    console.log('File was successfully opened', fileHandle)
+  }
+})
 
   // Callback hell example (test and leave it in comments):
 
 
   // 2. Promise style
-
+fsPromises.readFile(filePath, 'utf8')
+  .then((fileHandle) => {
+    console.log('File was successfully opened', fileHandle)
+    return fileHandle
+  })
+  .catch((err) => {
+    console.log('File failed to open', err)
+  })
 
       // 3. Async/Await style
+async function readFileAsync() {
+  try {
+    const fileHandle = await fsPromises.readFile(filePath, 'utf8')
+    console.log('File opened successfully', fileHandle)
+    return fileHandle
+  } catch (err) {
+    console.log('File failed to open', err)
+  }
+}
 
-      //Sample Addition
+readFileAsync()
